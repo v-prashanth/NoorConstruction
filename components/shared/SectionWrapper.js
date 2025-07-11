9416f0b2ab9b1121ae343c208930b03a97a57f91
Prashanth Vishwakarma <82275050+v-prashanth@users.noreply.github.com>
2025-07-11 12:37:24 +0530

    modified the mobile spacing in section wrapper

--- components/shared/SectionWrapper.js
+++ components/shared/SectionWrapper.js
@@ -1,20 +1,20 @@
-// export default function SectionWrapper({ children, className = "" }) {
-//   return (
-//     <section className={`w-full pt-28 md:pt-32 pb-20 md:pb-32 ${className}`}>
-//       <div className="max-w-7xl mx-auto px-4 md:px-8">
-//         {children}
-//       </div>
-//     </section>
-//   );
-// }
-
-////////
 export default function SectionWrapper({ children, className = "" }) {
   return (
-    <section className={`w-full pt-32 md:pt-32 pb-20 md:pb-32 ${className}`}>
-      <div className="max-w-7xl mx-auto px-4 md:px-8 mt-8 md:mt-0">
+    <section className={`w-full pt-28 md:pt-32 pb-20 md:pb-32 ${className}`}>
+      <div className="max-w-7xl mx-auto px-4 md:px-8">
         {children}
       </div>
     </section>
   );
 }
+
+////////
+// export default function SectionWrapper({ children, className = "" }) {
+//   return (
+//     <section className={`w-full pt-32 md:pt-32 pb-20 md:pb-32 ${className}`}>
+//       <div className="max-w-7xl mx-auto px-4 md:px-8 mt-8 md:mt-0">
+//         {children}
+//       </div>
+//     </section>
+//   );
+// }
